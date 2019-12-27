cc64e3cb221a1b4069af70124229c2573fd32014
Reema Patel <reemapatel17@outlook.com>
2019-12-27 12:29:06 +0000

    deployment - homepage appearing, still needs fixing

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, re_path
 from .views import Home, Assets
 
 urlpatterns = [
-  path('', Home.as_view())
+  path('', Home.as_view()),
   # re_path(r'^(?!/api)[\w\?\=\-\/]*$', Home.as_view())
-  # re_path(r'^(?P<filename>[\w\.\-\/]+)$', Assets.as_view())
-  # re_path(r'^[\w\?\=\-\/]*$', Home.as_view()),
+  re_path(r'^(?P<filename>[\w\.\-\/]+)$', Assets.as_view())
+  # re_path(r'^[\w\?\=\-\/]*$', Home.as_view())
   # re_path(r'(?P<filename>(assets/)?[\w\.\-\/]+)$', Assets.as_view())
-  # re_path(r'^[\w\?\=\-\/]*$', Home.as_view()),
+  # re_path(r'^[\w\?\=\-\/]*$', Home.as_view())
   # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
 ]
